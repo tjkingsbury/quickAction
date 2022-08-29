acb698903557213b6804c2016dac33a07255ccd6
Taylor Kingsbury <taylor@crmscience.com>
2022-08-29 11:54:40 -0400

    updating headless quick action

--- force-app/main/default/lwc/slackHeadlessQuickAction/slackHeadlessQuickAction.js
+++ force-app/main/default/lwc/slackHeadlessQuickAction/slackHeadlessQuickAction.js
@@ -1,24 +1,12 @@
-import { LightningElement, api, wire } from 'lwc';
-import { getRecord, getFieldValue } from 'lightning/uiRecordApi';
-import { updateRecord } from 'lightning/uiRecordApi';
-import { refreshApex } from '@salesforce/apex';
-import { CloseActionScreenEvent } from 'lightning/actions';
+import { LightningElement, api } from 'lwc';
 import { ShowToastEvent } from 'lightning/platformShowToastEvent';
-import ID_FIELD from '@salesforce/schema/Account.Id';
-import OWNERID_FIELD from '@salesforce/schema/Account.OwnerId';
-import RATING_FIELD from '@salesforce/schema/Account.Rating';
 import sendSlackMessage from '@salesforce/apex/SlackQuickActionController.sendSlackMessage';
-
-const FIELDS = [RATING_FIELD,OWNERID_FIELD];
 
 export default class SlackScreenQuickAction extends LightningElement {
     disabled = false;
     @api recordId;
     @api objectApiName;
     message = 'This account is red hot!';
-
-    @wire(getRecord, { recordId: '$recordId', fields: FIELDS })
-    account;
 
     @api invoke() {
         // Add your slack messaging call here
