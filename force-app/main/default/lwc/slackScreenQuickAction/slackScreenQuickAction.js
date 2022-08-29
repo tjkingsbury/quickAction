8c1038fd0cec119b0a8d778e19a7cd4308246c32
Taylor Kingsbury <taylor@crmscience.com>
2022-08-29 11:46:05 -0400

    add quick action

--- force-app/main/default/lwc/slackScreenQuickAction/slackScreenQuickAction.js
+++ force-app/main/default/lwc/slackScreenQuickAction/slackScreenQuickAction.js
@@ -1,24 +1,12 @@
-import { LightningElement, api, wire } from 'lwc';
-import { getRecord, getFieldValue } from 'lightning/uiRecordApi';
-import { updateRecord } from 'lightning/uiRecordApi';
-import { refreshApex } from '@salesforce/apex';
+import { LightningElement, api } from 'lwc';
 import { CloseActionScreenEvent } from 'lightning/actions';
 import { ShowToastEvent } from 'lightning/platformShowToastEvent';
-import ID_FIELD from '@salesforce/schema/Account.Id';
-import OWNERID_FIELD from '@salesforce/schema/Account.OwnerId';
-import RATING_FIELD from '@salesforce/schema/Account.Rating';
 import sendSlackMessage from '@salesforce/apex/SlackQuickActionController.sendSlackMessage';
 
-const FIELDS = [RATING_FIELD,OWNERID_FIELD];
-
 export default class SlackScreenQuickAction extends LightningElement {
-    disabled = false;
     @api recordId;
     @api objectApiName;
     message;
-
-    @wire(getRecord, { recordId: '$recordId', fields: FIELDS })
-    account;
 
     handleCancel(event) {
        this.dispatchEvent(new CloseActionScreenEvent());
@@ -26,7 +14,6 @@ export default class SlackScreenQuickAction extends LightningElement {
 
     handleSubmit(e) {
         // Add your slack messaging call here
-
         sendSlackMessage({recordId : this.recordId, message : this.message})
             .then(() =>{
                 this.dispatchEvent(
